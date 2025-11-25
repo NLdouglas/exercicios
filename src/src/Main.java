f430f171def331a880c0fa1e23468f4c82c54b58
NLdouglas <douglasleodoro.qi@gmail.com>
2025-11-25 00:57:49 -0300

    calc circulo

---
+++ src/src/Main.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Para calcular uma circulo vamos usar a formula A=piR ao quadrado");
+        System.out.println("Vamos usar o r ( raio ) com o numero 2, para calcular o raio faça RxR");
+        int calculoQuadrado = 7 *7;
+        double pi = 3.14;
+        System.out.println(calculoQuadrado + "depois  multiplique por Pi 3,14");
+        System.out.println(calculoQuadrado * pi + "cm");
+
+
+
+    }
+}
